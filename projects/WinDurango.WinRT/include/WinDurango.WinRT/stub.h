d5c08bf10fdb377c6349b8df211f365fb77181c5
Dexrn ZacAttack <me@dexrn.me>
2026-01-23 21:02:36 -0800

    refactor: move Storage to Common

---
+++ projects/WinDurango.WinRT/include/WinDurango.WinRT/stub.h
@@ -0,0 +1,9 @@
+//
+// Created by DexrnZacAttack on 1/23/26 using zPc-i2.
+//
+#ifndef WINDURANGO_STUB_H
+#define WINDURANGO_STUB_H
+
+// cant reload cmake without this
+
+#endif // WINDURANGO_STUB_H
