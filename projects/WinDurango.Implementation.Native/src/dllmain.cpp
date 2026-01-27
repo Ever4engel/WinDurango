6ad4250872217720b6708b9f1f6a76988ff2fc7a
Rodrigo Todescatto <90097545+Rodrigo-Todescatto@users.noreply.github.com>
2026-01-27 11:17:08 -0300

    SafetyHook fix

---
+++ projects/WinDurango.Implementation.Native/src/dllmain.cpp
@@ -0,0 +1,9 @@
+#include "../include/WinDurango.Implementation.Native/WinDurangoNative.h"
+#include <Windows.h>
+
+BOOL WINAPI DllMain(HINSTANCE hinstDLL,
+                    DWORD fdwReason,
+                    LPVOID lpvReserved)
+{
+    return TRUE;
+}
