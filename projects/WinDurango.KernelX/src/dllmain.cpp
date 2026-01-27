f2ac41a06aad18c98328b8f3d2993c883363b037
Rodrigo Todescatto <90097545+Rodrigo-Todescatto@users.noreply.github.com>
2026-01-27 11:36:02 -0300

    RoGetActivationFactory Hooking

---
+++ projects/WinDurango.KernelX/src/dllmain.cpp
@@ -0,0 +1,32 @@
+#include "kernelx.h"
+#include "Hooks.h"
+
+static DWORD ReasonForCall = 0;
+
+void KernelxInitialize(HINSTANCE hinstDLL)
+{
+    if (ReasonForCall == DLL_PROCESS_ATTACH || ReasonForCall == DLL_THREAD_ATTACH)
+    {
+        XWinePatchImport(GetModuleHandleW(nullptr),
+                         GetRuntimeModule(),
+                         "?GetActivationFactoryByPCWSTR@@YAJPEAXAEAVGuid@Platform@@PEAPEAX@Z",
+                         GetActivationFactoryRedirect);
+    }
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
+{
+    InitializeCriticalSection(&XmpAllocationHookLock);
+
+    ReasonForCall = ul_reason_for_call;
+
+    if (ul_reason_for_call != DLL_PROCESS_ATTACH)
+    {
+        ReasonForCall = ul_reason_for_call;
+        return TRUE;
+    }
+
+    return QueueUserAPC(
+               [](ULONG_PTR dwParam) { KernelxInitialize(reinterpret_cast<HINSTANCE>(dwParam)); },
+               GetCurrentThread(), reinterpret_cast<ULONG_PTR>(hModule)) != 0;
+}
